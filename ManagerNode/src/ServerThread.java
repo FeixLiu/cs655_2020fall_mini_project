e00d49a17d1df87e5bfec944a55da3ed54ee0790
Felix <yaliu@bu.edu>
2020-11-28 12:10:47 -0500

    test

--- ManagerNode/src/ServerThread.java
+++ ManagerNode/src/ServerThread.java
@@ -27,11 +27,11 @@ public class ServerThread implements Runnable{
 
             String response = "";
             response += "HTTP/1.1 200 OK\n";
-            response += "Server: Sunpache 1.0\n";
-            response += "Content-Type: text\\html\n";
-            response += "Last-Modified: Mon, 11 Jan 1998 13:23:42 GMT\n";
-            response += "Accpet-ranges: bytes";
-            response += "Content-Length: " + res.length();
+//            response += "Server: Sunpache 1.0\n";
+            response += "Content-Type: text\\plain\n";
+//            response += "Last-Modified: Mon, 11 Jan 1998 13:23:42 GMT\n";
+//            response += "Accpet-ranges: bytes";
+//            response += "Content-Length: " + res.length();
             response += "\n\n";
             response += res;
 
