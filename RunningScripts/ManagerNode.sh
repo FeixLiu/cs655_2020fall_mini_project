fee69e590debcfaec69ba9c74bcdf46ffcd21b7f
Felix <yaliu@bu.edu>
2020-12-02 17:27:14 -0500

    udpate bash

--- RunningScripts/ManagerNode.sh
+++ RunningScripts/ManagerNode.sh
@@ -4,7 +4,7 @@ sudo apt install default-jdk
 sudo apt install nginx
 sudo rm -rf cs655_2020fall_mini_project-password_cracker_v1.0/
 sudo rm -f password_cracker_v1.0.tar.gz
-sudo wget --no-check-certificate --no-cache --no-cookies https://github.com/FeixLiu/cs655_2020fall_mini_project/archive/password_cracker_v1.0.tar.gz
+sudo wget --no-check-certificate --no-cache --no-cookies https://github.com/FeixLiu/cs655_2020fall_mini_project/archive/password_cracker_v1.0.tar.gz --post-data="action=purge"
 sudo tar -zvxf password_cracker_v1.0.tar.gz
 sudo touch /etc/nginx/conf.d/static-naice-me.conf
 sudo echo 'server {' > /etc/nginx/conf.d/static-naice-me.conf
--- RunningScripts/WorkerNode.sh
+++ RunningScripts/WorkerNode.sh
@@ -1,10 +1,9 @@
 sudo apt update
 sudo apt install default-jre
 sudo apt install default-jdk
-sudo apt install nginx
 sudo rm -rf cs655_2020fall_mini_project-password_cracker_v1.0/
 sudo rm -f password_cracker_v1.0.tar.gz
-sudo wget --no-check-certificate --no-cache --no-cookies https://github.com/FeixLiu/cs655_2020fall_mini_project/archive/password_cracker_v1.0.tar.gz
+sudo wget --no-check-certificate --no-cache --no-cookies https://github.com/FeixLiu/cs655_2020fall_mini_project/archive/password_cracker_v1.0.tar.gz  --post-data="action=purge"
 sudo tar -zvxf password_cracker_v1.0.tar.gz
 cd cs655_2020fall_mini_project-password_cracker_v1.0/WorkNode
 sudo javac Main.java
